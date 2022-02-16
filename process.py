1e7e489eb8ff1378029d61167701ea4d1c60180f
Nydia Medina <nvmedina29@gmail.com>
2022-02-16 05:32:25 -0600

    Submitting Part 1

--- process.py
+++ process.py
@@ -1,12 +1,17 @@
+# Open "um-server-01.txt" file and store it in variable log_file
 log_file = open("um-server-01.txt")
 
-
+# Define function sales_reports that accepts a log_file as parameter
 def sales_reports(log_file):
+    # For each line in log_file, whitespaces will be removed, and the 
+    # first three chars of each line are stored in day
     for line in log_file:
         line = line.rstrip()
         day = line[0:3]
-        if day == "Tue":
+        # If day is equal to "Mon", line will be printed
+        if day == "Mon":
             print(line)
 
-
+# Function sales_report is called with the log_file from "um-server-01.txt"
+# as parameter
 sales_reports(log_file)
